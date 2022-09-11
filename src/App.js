94e7f35dd9ee6965d4be59850bf84cc756242b05
Kaung Myat Tun <kaungtun111@gmail.com>
2022-09-11 23:09:34 +0630

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit'
+import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide'
 
 function App() {
-  
+ 
   return (
     <div className='container'>
       <Setup />
